102743786da31ad68e98ee3e09b3f64992278251
OGCRAFTNET STUDIOS <simonnintendo12@outlook.com>
2025-05-06 18:11:59 +0200

    Add files via upload

---
+++ lib/pbr/blocks/flowers.glsl
@@ -0,0 +1,16 @@
+if (material >= 35 && material <= 40) {
+	if (albedo.r + albedo.g + albedo.b < 2.7) {
+		if (albedo.b > albedo.g || albedo.r > albedo.g) {
+			emission = lAlbedo;
+		}
+	}
+	if (material == 39 && lAlbedo > 0.6) {
+		emission = lAlbedo * lAlbedo;
+	}
+
+	emission = max(emission, 0.0);
+
+	#ifdef OVERWORLD
+	emission *= mix(0.1, 0.0, wetness);
+	#endif	
+}
